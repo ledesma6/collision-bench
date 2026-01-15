57b3a2d1b2b789aa7cc066f8d9570b1ee16ad17d
Mike Ledesma <245998916+ledesma6@users.noreply.github.com>
2026-01-15 01:21:35 -0500

    feat: add Implementation of AABB
    * Add implementation of an Axis-Aligned Bounding Box; update main loop to generate a box with 1000 points.
    * Add unit tests for the AABB class

---
+++ include/Point.hpp
@@ -0,0 +1,10 @@
+#ifndef COLLISION_BENCH_POINT_HPP
+#define COLLISION_BENCH_POINT_HPP
+#include <Eigen/Dense>
+
+namespace collision {
+    using Vector3 = Eigen::Vector3d;
+}
+
+
+#endif //COLLISION_BENCH_POINT_HPP
